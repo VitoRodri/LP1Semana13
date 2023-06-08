9721f54d93750eab3e4e762d375abcdded20f1d5
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-06-08 23:50:46 +0100

    Add method Palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+
+        private bool Palindrome(string s)
+        {
+            bool IsString(string s)
+            {
+                char[] array=s.ToCharArray();
+                Array.Reverse(array);
+                    
+                    
+                if (s==new string(array))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+
+            if (s==null)
+            {
+                throw new ArgumentNullException("String cant be null");
+            }
+            else if(s.Length<2)
+            {
+                return true;
+            }
+            else
+            {
+                return IsString(s);
+            }
+            
+        }
+    }
+}
