208eb499775fa9496a33c75950036b0952501f65
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-06-09 00:21:06 +0100

    Add SuperList

---
+++ MultipleStuff/SuperList.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultipleStuff
+{
+    public class SuperList: List<double>
+    {
+        public double GetMinMax1(out List<double> list)
+        {
+            return(list.Min(),list.Max());
+        }
+        struct SuperList
+        {
+            public GetMinMax2(List<double> list)
+            {
+                return(list.Min(),list.Max());
+            }
+        } 
+        public static Tuple<double,double> GetMinMax3(List<double> list)
+        {
+            return Tuple.Create(list.Min(),list.Max());
+        }
+        public static (double min, double max) GetMinMax4(List<double> list)
+        {
+            return(list.Min(),list.Max());
+        }
+    }
+}
